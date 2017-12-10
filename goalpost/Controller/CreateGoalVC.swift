04a297b4ee6dd2756ec6e7aa5ee5a63bd0e10b1f
Sergiy Sachuk <sergiysachuk@Sergiys-MacBook-Pro.local>
2017-12-10 14:18:46 +0200

    new CreateGoalVC, outlets and IBActions

---
+++ goalpost/Controller/CreateGoalVC.swift
@@ -0,0 +1,36 @@
+//
+//  CreateGoalVC.swift
+//  goalpost
+//
+//  Created by Sergiy Sachuk on 12/10/17.
+//  Copyright © 2017 Sergiy Sachuk. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shotTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func nextBrnPressed(_ sender: Any) {
+    }
+    @IBAction func shortTermBtnPressed(_ sender: Any) {
+    }
+    @IBAction func longTermBtnPressed(_ sender: Any) {
+    }
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
